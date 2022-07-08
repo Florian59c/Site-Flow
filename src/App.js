6d14366783c6a16d2c7ce35a4d2072db8a939a38
Florian Cagnon <florian.cagnon@live.fr>
2022-07-08 18:56:29 +0200

    ajout du css pour les 2 cotes de la page d'accueil + début de styles du bouton mode light

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { useState } from 'react';
-import { Button, Col, Row } from 'antd';
-import flow from './img/Flow.png'
-import loup from './img/Loup.jpg'
+import { Col, Row } from 'antd';
+import flow from './img/Flow.png';
+import loup from './img/Loup.png';
+import jour from './img/Ciel-bleu.jpg';
+import nuit from './img/Ciel-nuit.jpg';
 
 export default function App() {
 
@@ -21,43 +23,49 @@ export default function App() {
   // };
 
   return (
-    <div className='accueil'>
-      <div>
-        {/* <img
+    <div className='accueil' style={{ backgroundImage: isDark ? `url(${nuit})` : `url(${jour})` }}>
+      <div className='contenuAccueil'>
+        <div>
+          {/* <img
           style={{ background: isDark ? '' : 'radial-gradient(ellipse farthest-corner,rgb(176,218,240), rgb(0,0,0, 0) 30% )', }}
           src={isDark ? loup : flow} alt="cercle d'eau en mouvement"
         /> */}
 
-        {isDark ?
-          <img style={{ background: '' }} src={loup} alt="cercle d'eau en mouvement" />
-          :
-          <img style={{ background: 'radial-gradient(ellipse farthest-corner,rgb(176,218,240), rgb(0,0,0, 0) 30% )' }} src={flow} alt="image d'un loup hurlant à la lune" />
-        }
+          {isDark ?
+            <img style={{ background: '' }} src={loup} alt="cercle d'eau en mouvement" />
+            :
+            <img style={{ background: 'radial-gradient(ellipse farthest-corner,rgb(176,218,240), rgb(0,0,0, 0) 30% )' }} src={flow} alt="image d'un loup hurlant à la lune" />
+          }
 
 
+        </div>
+        <div>
+          <Row justify="center" style={{ flexDirection: isDark ? 'row-reverse' : '', }} >
+            <Col>
+              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>F</h1>
+            </Col>
+            <Col>
+              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>L</h1>
+            </Col>
+            <Col>
+              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>O</h1>
+            </Col>
+            <Col>
+              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>W</h1>
+            </Col>
+          </Row>
+        </div>
+        <div>
+          <h2 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>
+            {isDark ? 'was appeared !' : 'vous présente ses projets'}
+          </h2>
+        </div>
+        <button className='bouton' onClick={changeMode} 
+        
+        >
+          {isDark ? <p>Light Mode ?</p> : <p>Dark Mode</p>}
+        </button>
       </div>
-      <div>
-        <Row justify="center" style={{ flexDirection: isDark ? 'row-reverse' : '', }} >
-          <Col>
-            <h1 style={{ color: isDark ? 'white' : 'black', }}>F</h1>
-          </Col>
-          <Col>
-            <h1 style={{ color: isDark ? 'white' : 'black', }}>L</h1>
-          </Col>
-          <Col>
-            <h1 style={{ color: isDark ? 'white' : 'black', }}>O</h1>
-          </Col>
-          <Col>
-            <h1 style={{ color: isDark ? 'white' : 'black', }}>W</h1>
-          </Col>
-        </Row>
-      </div>
-      <div>
-        <h2 style={{ color: isDark ? 'white' : 'black', }}>
-          {isDark ? 'appeared' : 'vous présente ses projets'}
-        </h2>
-      </div>
-      <Button onClick={changeMode} >Dark Mode</Button>
     </div>
   )
 }
