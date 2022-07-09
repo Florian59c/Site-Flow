6c4db937d9ca8da31c65a1b32ca590b211618b17
Florian Cagnon <florian.cagnon@live.fr>
2022-07-09 17:25:38 +0200

    optimisation du code et des ternaires : remplacement des styles sur les balises par des classes pour le css

--- src/App.js
+++ src/App.js
@@ -33,40 +33,33 @@ export default function App() {
       <div className='contenuAccueil'>
         <div>
           {isDark ?
-            <img style={{ background: '' }} src={loup} alt="cercle d'eau en mouvement" />
+            <img src={loup} alt="cercle d'eau en mouvement" />
             :
-            <img style={{ background: 'radial-gradient(ellipse farthest-corner,rgb(176,218,240), rgb(0,0,0, 0) 30% )' }} src={flow} alt="image d'un loup hurlant à la lune" />
+            <img className='imgLight' src={flow} alt="image d'un loup hurlant à la lune" />
           }
         </div>
         <div>
           <Row justify="center" style={{ flexDirection: isDark ? 'row-reverse' : '', }} >
             <Col>
-              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>F</h1>
+              <h1 className={`${isDark ? "titreDark" : "titreLight"}`}>F</h1>
             </Col>
             <Col>
-              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>L</h1>
+              <h1 className={`${isDark ? "titreDark" : "titreLight"}`}>L</h1>
             </Col>
             <Col>
-              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>O</h1>
+              <h1 className={`${isDark ? "titreDark" : "titreLight"}`}>O</h1>
             </Col>
             <Col>
-              <h1 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>W</h1>
+              <h1 className={`${isDark ? "titreDark" : "titreLight"}`}>W</h1>
             </Col>
           </Row>
         </div>
         <div>
-          <h2 style={{ color: isDark ? 'white' : '#22211F', textShadow: isDark ? '-0.5rem 0.5rem 1rem #E0E0E0' : '-0.5rem 0.5rem 1rem #22211F', }}>
+          <h2 className={`${isDark ? "titreDark" : "titreLight"}`}>
             {isDark ? 'was appeared !' : 'vous présente ses projets'}
           </h2>
         </div>
-        <button className='bouton' onClick={changeMode} onMouseEnter={changeHover} onMouseLeave={returnHover}
-          style={{
-            color: isDark ? 'white' : 'black',
-            border: isDark ? '0.2rem solid rgb(208, 208, 211)' : '0.2rem solid rgb(144, 147, 163)',
-            backgroundColor: isDark ? '#bababd' : '#444751',
-            boxShadow: isDark ? '0 1rem 1rem 0 rgb(255, 255, 255, 0.4)' : '0 1rem 1rem 0 rgb(0, 0, 0, 0.3)',
-            background: isDark ? 'linear-gradient(rgb(209, 209, 209), rgb(70, 70, 70))' : 'linear-gradient(rgb(99, 99, 110), rgb(59, 61, 70))'
-          }}
+        <button className={`bouton ${isDark ? "boutonDark" : "boutonLight"}`} onClick={changeMode} onMouseEnter={changeHover} onMouseLeave={returnHover}
         >
           {isDark ? <p>Light Mode ?</p> : <p>Dark Mode</p>}
         </button>
