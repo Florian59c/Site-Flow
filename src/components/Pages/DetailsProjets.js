2e8451b47bc661f4cf11c444591c9e602ca2e5b5
Florian Cagnon <florian.cagnon@live.fr>
2022-07-23 18:46:12 +0200

    recupération des données des props pour la page DetailsProjets

--- src/components/Pages/DetailsProjets.js
+++ src/components/Pages/DetailsProjets.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import ListeProjets from '../Classes/ListeProjets';
 
 const DetailsProjets = () => {
+
+  const { url } = useParams();
+
+  const projetTrouver = ListeProjets.find(projet => {
+    return projet.url === url
+  })
+
   return (
     <div>
-      <h1>DetailsProjets</h1>
+      <h1>{projetTrouver.id}</h1>
+      <h1>{projetTrouver.titre}</h1>
     </div>
   )
 }
