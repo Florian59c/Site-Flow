5d1a54f2f63c0e2e129e676de454c1327e4b16aa
Florian Cagnon <florian.cagnon@live.fr>
2022-07-24 19:09:01 +0200

    ajout d'une v1 dynamique de la page DétailsProjets (non responsive)

--- src/components/Classes/ListeProjets.js
+++ src/components/Classes/ListeProjets.js
@@ -1,38 +1,73 @@
 const ListeProjets = [
     {
         id: 0,
+        nom: "Le site de Flow",
         titre: "Présentation du site",
-        url: "presentation-du-site"
+        url: "presentation-du-site",
+        lienImage: "https://www.radiolac.ch/wp-content/uploads/2022/07/20220723203238492.jpg",
+        listLangages: "React / Git",
+        presentation: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus inventore, sunt laboriosam facere molestias unde ea nisi nihil atque quia obcaecati libero ab, reprehenderit mollitia assumenda, fugiat quam sint magni.",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus inventore, sunt laboriosam facere molestias unde ea nisi nihil atque quia obcaecati libero ab, reprehenderit mollitia assumenda, fugiat quam sint magni."
     },
     {
         id: 1,
+        nom: "???",
         titre: "En cours...",
-        url: "en-cours"
+        url: "en-cours",
+        lienImage: "",
+        listLangages: "",
+        presentation: "",
+        details: ""
     },
     {
         id: 2,
+        nom: "projets 1",
         titre: "projets 1",
-        url: "projets-1"
+        url: "projets-1",
+        lienImage: "",
+        listLangages: "",
+        presentation: "",
+        details: ""
     },
     {
         id: 3,
+        nom: "projets 2",
         titre: "projets 2",
-        url: "projets-2"
+        url: "projets-2",
+        lienImage: "",
+        listLangages: "",
+        presentation: "",
+        details: ""
     },
     {
         id: 4,
+        nom: "projets 3",
         titre: "projets 3",
-        url: "projets-3"
+        url: "projets-3",
+        lienImage: "",
+        listLangages: "",
+        presentation: "",
+        details: ""
     },
     {
         id: 5,
+        nom: "projets 4",
         titre: "projets 4",
-        url: "projets-4"
+        url: "projets-4",
+        lienImage: "",
+        listLangages: "",
+        presentation: "",
+        details: ""
     },
     {
         id: 6,
+        nom: "projets 5",
         titre: "projets 5",
-        url: "projets-5"
+        url: "projets-5",
+        nomlienImage: "",
+        listLangages: "",
+        presentation: "",
+        details: ""
     }
 ];
 
--- src/components/Pages/DetailsProjets.js
+++ src/components/Pages/DetailsProjets.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Header from '../Composants/Header';
+import Footer from '../Composants/Footer';
 import ListeProjets from '../Classes/ListeProjets';
-
+// projetTrouver.nomImage
 const DetailsProjets = () => {
 
+  // recupere dans mes params de mes props la valeur de l'url de la page
   const { url } = useParams();
 
+  // fonction qui compare l'url recuperé juste avant et le compare avec les url de chaque projets dans ma ListeProjets - une fois trouvé, la fonction ne garde qu'un tableau contenant les information d'un seul projet (celui qui a le bon url)
   const projetTrouver = ListeProjets.find(projet => {
     return projet.url === url
   })
 
   return (
     <div>
-      <h1>{projetTrouver.id}</h1>
-      <h1>{projetTrouver.titre}</h1>
+      <Header />
+      <div className='projetsContainer'>
+        <div>
+          <h1>{projetTrouver.nom}</h1>
+        </div>
+        <div>
+          <div>
+            <img src={projetTrouver.lienImage} alt="Capture décran du projet" />
+            <p>{projetTrouver.listLangages}</p>
+          </div>
+          <div>
+            <h2>Présentation</h2>
+            <p>{projetTrouver.presentation}</p>
+          </div>
+        </div>
+        <div>
+          <h2>plus de détails</h2>
+          <p>{projetTrouver.details}</p>
+        </div>
+        <div>
+          <h3>Pour avoir encore plus d'informations, vous pouvez lire la documentation complète du projet ou le tester grâce aux boutons suivants !</h3>
+        </div>
+        <div>
+          <div className='boutonProjets'>
+            <button>
+              <h1>
+                Documentation sur {projetTrouver.nom}
+              </h1>
+            </button>
+          </div>
+          <div className='boutonProjets'>
+            <button>
+              <h1>
+                Découvrir {projetTrouver.nom}
+              </h1>
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
