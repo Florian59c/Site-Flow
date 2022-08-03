35950323b8bf22c0275dce678143a77ebdb8deb0
Florian Cagnon <florian.cagnon@live.fr>
2022-08-03 18:35:32 +0200

    ajout des styles de la page NotFound

--- src/components/Pages/NotFound.js
+++ src/components/Pages/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import MenInBlack from '../../img/Men-in-black.png';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='notFoundContainer'>
       <h1>404 Page non trouvée</h1>
-      <img src={MenInBlack} alt="Homme en costume cravate sur fond blanc" />
+      <Link to={"/"}>
+        <img src={MenInBlack} alt="Homme en costume cravate sur fond blanc" />
+      </Link>
       <h1>Circulez, y a rien à voir ici !</h1>
     </div>
   )
